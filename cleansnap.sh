b2d513ea2446f77728aa9f3efe393b456b37e6c9
lingyicute <160479206+lingyicute@users.noreply.github.com>
2025-01-28 19:49:56 +0800

    Update cleansnap.sh

--- cleansnap.sh
+++ cleansnap.sh
@@ -4,7 +4,7 @@
 LOG_FILE="/root/cleansnap-script-log"
 
 # 记录脚本开始运行时间
-echo "脚本运行时间: $(date)" >> "$LOG_FILE"
+echo "Script start time 脚本运行时间: $(date)" >> "$LOG_FILE"
 
 # 确保脚本在出错时停止
 set -eu
@@ -14,15 +14,15 @@ LANG=en_US.UTF-8
 # 遍历所有已禁用的 Snap 并删除，同时记录删除的 Snap 到日志中
 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        echo "删除 Snap: $snapname, 修订版本: $revision" >> "$LOG_FILE"
+        echo "Deleting Snap: $snapname, Revision 修订版本: $revision" >> "$LOG_FILE"
         snap remove "$snapname" --revision="$revision"
         if [ $? -eq 0 ]; then
-            echo "成功删除: $snapname, 修订版本: $revision" >> "$LOG_FILE"
+            echo "Successfully deleted 成功删除: $snapname, Revision 修订版本: $revision" >> "$LOG_FILE"
         else
-            echo "删除失败: $snapname, 修订版本: $revision" >> "$LOG_FILE"
+            echo "Failed to delete 删除失败: $snapname, Revision 修订版本: $revision" >> "$LOG_FILE"
         fi
     done
 
 # 记录脚本结束运行时间
-echo "脚本运行结束时间: $(date)" >> "$LOG_FILE"
+echo "Script end time 脚本运行结束时间: $(date)" >> "$LOG_FILE"
 echo "-------" >> "$LOG_FILE"
